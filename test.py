98c9bbd1f4f09c270ac33c4263114cfa43ba5c06
issakha07 <zackserra@gmail.com>
2021-05-06 14:40:25 +0000

    Premier code avec python

---
+++ test.py
@@ -0,0 +1,41 @@
+# View
+print("DefarScie Naga deff")
+
+# Les types de données dans python
+type_donnee = "Types de données -> integer : les nombres ; floatt : les decimaux 2.0 ; string : chaine de caractére"
+print(type_donnee)
+
+# Variable
+prenom = "Mamadou Issakha"
+nom = "Sarr"
+print(prenom + ' ' + nom)
+
+# Les opérateurs mathématiques
+nombre_1 = 10
+nombre_2 = 5
+addition = nombre_1 + nombre_2
+soustraction = nombre_1 - nombre_2
+multiplication = nombre_1 * nombre_2
+division = nombre_1 / nombre_2
+puissance = nombre_1 ** 2
+exponent = nombre_1 ** nombre_2
+
+
+print("Résultat_Addition=", addition)
+print("Résultat_Soustraction=", soustraction)
+print("Résultat_Multiplication=", multiplication)
+print("Résultat_Division=", division)
+print("Résultat_Puissance=", puissance)
+print("Résultat_Exponent=", exponent)
+
+# Fonctions
+print("Note maximum", max (15, 7, 20, 11))
+
+
+
+
+
+
+
+
+
